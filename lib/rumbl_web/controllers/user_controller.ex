0403aa6fac13635c97aed413baa11958f306995e
Gerald Annan <annang2@stanbic.com.gh>
2022-02-27 19:51:47 +0000

    perfrom validation for insert outcomes

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -1,14 +1,15 @@
 defmodule RumblWeb.UserController do
   use RumblWeb, :controller
   alias RumblWeb.Models.User
+  alias Rumbl.Repo
 
   def index(conn, _params) do
-    users = Rumbl.Repo.all(User)
+    users = Repo.all(User)
     render(conn, "index.html", users: users)
   end
 
   def show(conn, %{"id" => id}) do
-    user = Rumbl.Repo.get(User, id)
+    user = Repo.get(User, id)
     render(conn, "show.html", user: user)
   end
 
@@ -19,9 +20,13 @@ defmodule RumblWeb.UserController do
 
   def create(conn, %{"user" => user_params}) do
     changeset = User.changeset(%User{}, user_params)
-    {:ok, user} = Repo.insert(changeset)
-    conn
-    |> put_flash(:info, "#{user.name} created!")
-    |> redirect(to: user_path(conn, :index))
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
