08638e070a625d78119c89432ee10da9b73034ae
Gerald Annan <annang2@stanbic.com.gh>
2022-02-27 19:44:30 +0000

    implement create user function

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -1,18 +1,27 @@
 defmodule RumblWeb.UserController do
   use RumblWeb, :controller
+  alias RumblWeb.Models.User
 
   def index(conn, _params) do
-    users = Rumbl.Repo.all(RumblWeb.Models.User)
+    users = Rumbl.Repo.all(User)
     render(conn, "index.html", users: users)
   end
 
   def show(conn, %{"id" => id}) do
-    user = Rumbl.Repo.get(RumblWeb.Models.User, id)
+    user = Rumbl.Repo.get(User, id)
     render(conn, "show.html", user: user)
   end
 
   def new(conn, _params) do
-    changeset = RumblWeb.Models.User.changeset(%RumblWeb.Models.User{})
+    changeset = User.changeset(%User{})
     render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    {:ok, user} = Repo.insert(changeset)
+    conn
+    |> put_flash(:info, "#{user.name} created!")
+    |> redirect(to: user_path(conn, :index))
   end
 end
